1e5e9b8e3781656400a26b2c613dd8956445c609
Dhruvin-Purohit <paragdhruvincoc@gmail.com>
2021-01-23 10:28:14 +0530

    I hope this fixes the config.ts?

--- Bot/Data/config.ts
+++ Bot/Data/config.ts
@@ -2,6 +2,7 @@
  * I know this will error because typescript will try to find the devconfig file when we want to keep it optional.
  * I am looking for a workaround this. If you have any ideas, kindly open an issue or pull request.* * * * * * * *
  */
+// @ts-ignore
 import devconfig from './config-dev';
 import safeconfig from './safe-config';
 
