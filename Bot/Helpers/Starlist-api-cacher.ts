c50339a8796cdeed59760db8fcbbda94a5a45d0b
Dhruvin-Purohit <paragdhruvincoc@gmail.com>
2021-01-24 10:27:26 +0530

    Lowered the caching time and in b/w time, ratelimits of 3/s isn't too less after all

--- Bot/Helpers/Starlist-api-cacher.ts
+++ Bot/Helpers/Starlist-api-cacher.ts
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 let Data = new Map();
 
 export default class StarlistAPICacher extends EventEmitter {
-	public constructor(key: string, cachingTime: number = 5 * 60 * 1000) {
+	public constructor(key: string, cachingTime: number = 2 * 60 * 1000) {
 		super();
 		this._starlist_key = key;
 		this._cachingTime = cachingTime;
@@ -38,27 +38,27 @@ export default class StarlistAPICacher extends EventEmitter {
 		let events = await this._fetch('events');
 		this.data.set('events', events);
 
-		await this._sleep(5 * 1000);
+		await this._sleep(1 * 1000);
 
 		let brawlers = await this._fetch('brawlers');
 		this.data.set('brawlers', brawlers);
 
-		await this._sleep(5 * 1000);
+		await this._sleep(1 * 1000);
 
 		let icons = await this._fetch('icons');
 		this.data.set('icons', icons);
 
-		await this._sleep(5 * 1000);
+		await this._sleep(1 * 1000);
 
 		let maps = await this._fetch('maps');
 		this.data.set('maps', maps);
 
-		await this._sleep(5 * 1000);
+		await this._sleep(1 * 1000);
 
 		let gamemodes = await this._fetch('gamemodes');
 		this.data.set('gamemodes', gamemodes);
 
-		await this._sleep(5 * 1000);
+		await this._sleep(1 * 1000);
 
 		if (!this.is_ready) {
 			this.is_ready = true;
