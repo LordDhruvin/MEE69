bc4c21bedb0aae1bca1ef5014e838b899a0db769
Dhruvin-Purohit <paragdhruvincoc@gmail.com>
2021-01-24 16:30:55 +0530

    Added club command & what else? (beautified the code)

--- Bot/Base/Brawl Stars Commands/Satistics/club.ts
+++ Bot/Base/Brawl Stars Commands/Satistics/club.ts
@@ -1,1 +1,62 @@
-// @ts-ignore
+import { Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+import SupercellGameStatsUtil from '../../../Helpers/SupercellGameStatsUtil';
+
+//Both commands are very similar ;)
+export default class extends Command {
+	public constructor() {
+		super('brawl_command_statistics_club', {
+			aliases: ['club'],
+			args: [
+				{
+					id: 'tag',
+					type: 'string',
+					prompt: {
+						start:
+							"What club's profile do you want me to show?(Give me the club's tag)",
+					},
+				},
+			],
+			typing: true
+		});
+	}
+
+	public async exec(message: Message, { tag }: { tag: string }) {
+		//All below this will become a custom arguement type, tryng to learn to do that...
+		//Looking to do it as a regex that captures only the tag (without the #) and only contains those characters.
+		tag = tag.toUpperCase();
+		tag = tag.replace('#', '')
+		let tagCanOnlyContain = [
+			'0',
+			'2',
+			'8',
+			'9',
+			'P',
+			'Y',
+			'L',
+			'Q',
+			'G',
+			'R',
+			'J',
+			'C',
+			'U',
+			'V',
+		];
+		tagCanOnlyContain//I love ts :)
+		tag = tag.replace('O', '0');
+			let clubStats = await SupercellGameStatsUtil.getClubBrawlStarsStatsById(
+				tag
+			);
+			if (clubStats.status === 400) {
+				return message.util?.send(
+					`Club not found!\nMost probably the given club tag is incorrect.`
+				);
+			} else if (clubStats.status === 200 && clubStats.embed) {
+				return message.util?.send(clubStats.embed);
+			} else {
+				return message.util?.send(
+					`There is some unknown error with this command.\nMaybe you should try again later.`
+				);
+			}
+	}
+}
--- Bot/Helpers/SupercellGameStatsUtil.ts
+++ Bot/Helpers/SupercellGameStatsUtil.ts
@@ -3,7 +3,7 @@ import { MessageEmbed } from 'discord.js';
 import config from '../Data/config';
 import index from '../index';
 import emojis from '../Data/emojis';
-import Utils from './Utils'
+import Utils from './Utils';
 
 /**
  * Get's stats of the player from the api, makes an embed, and returns embed and status of the result.
@@ -14,38 +14,40 @@ async function getPlayerBrawlStarsStatsById(playerTag: string) {
 		`https://bsproxy.royaleapi.dev/v1/players/%23${playerTag}`,
 		{ headers: { Authorization: `Bearer ${config.keys.supercell.bs}` } }
 	);
-    const stats = await res.json();
-    let embed = null
-    if(res.status === 200) {
-	let totalTrophies =
-		stats.soloVictories + stats.duoVictories + stats['3vs3Victories'];
-	let brawlers = index.starlistapicache?.data.get('brawlers');
-	embed = new MessageEmbed()
-		.addField(
-			`${Utils.getPlayerBadgeEmoteById(stats.icon.id)}${stats.name} | ${stats.tag}`,
-			`**Trophies:** \`${stats.trophies || '-'}/${
-				stats.highestTrophies || '-'
-			}\`${emojis.bsem2.Trophy}\n` +
-				`**Power Play:** \`${stats.powerPlayPoints || '-'}/${
-					stats.highestPowerPlayPoints || '-'
-				}\`${emojis.bsem2.PowerPlayPoints}\n` +
-				`**Solo Victories:** \`${
-					stats.soloVictories || '-'
-				}/${totalTrophies}\`${emojis.bsem2.SoloShowdown}\n` +
-				`**Duo Victories:** \`${
-					stats.duoVictories || '-'
-				}/${totalTrophies}\`${emojis.bsem2.DuoShowdown}\n` +
-				`**3v3 Victories:** \`${
-					stats['3vs3Victories'] || '-'
-				}/${totalTrophies}\`${emojis.bsem2['3v3Victories']}\n` +
-				`**Brawlers:** \`${stats.brawlers.length}/${
-					brawlers.list.length || '-'
-				}\``,
-			true
-		)
-		.addField(`Club`, `${stats.club.name} | ${stats.club.tag}`)
-		.setColor('#' + stats.nameColor.slice(4));
-            }
+	const stats = await res.json();
+	let embed = null;
+	if (res.status === 200) {
+		let totalTrophies =
+			stats.soloVictories + stats.duoVictories + stats['3vs3Victories'];
+		let brawlers = index.starlistapicache?.data.get('brawlers');
+		embed = new MessageEmbed()
+			.addField(
+				`${Utils.getPlayerBadgeEmoteById(stats.icon.id)}${
+					stats.name
+				} | ${stats.tag}`,
+				`**Trophies:** \`${stats.trophies || '-'}/${
+					stats.highestTrophies || '-'
+				}\`${emojis.bsem2.Trophy}\n` +
+					`**Power Play:** \`${stats.powerPlayPoints || '-'}/${
+						stats.highestPowerPlayPoints || '-'
+					}\`${emojis.bsem2.PowerPlayPoints}\n` +
+					`**Solo Victories:** \`${
+						stats.soloVictories || '-'
+					}/${totalTrophies}\`${emojis.bsem2.SoloShowdown}\n` +
+					`**Duo Victories:** \`${
+						stats.duoVictories || '-'
+					}/${totalTrophies}\`${emojis.bsem2.DuoShowdown}\n` +
+					`**3v3 Victories:** \`${
+						stats['3vs3Victories'] || '-'
+					}/${totalTrophies}\`${emojis.bsem2['3v3Victories']}\n` +
+					`**Brawlers:** \`${stats.brawlers.length}/${
+						brawlers.list.length || '-'
+					}\``,
+				true
+			)
+			.addField(`Club`, `${stats.club.name} | ${stats.club.tag}`)
+			.setColor('#' + stats.nameColor.slice(4));
+	}
 	return {
 		status: res.status,
 		result: res,
@@ -58,34 +60,71 @@ async function getPlayerBrawlStarsStatsById(playerTag: string) {
  * @param clubTag Brawl Stars club tag of the brawl stars Club
  */
 async function getClubBrawlStarsStatsById(clubTag: string) {
-	const res = await fetch(`https://bsproxy.royaleapi.dev/v1/clubs/%23${clubTag}`, { headers: {'Authorization': `Bearer ${config.keys.supercell.bs}`}})
+	const res = await fetch(
+		`https://bsproxy.royaleapi.dev/v1/clubs/%23${clubTag}`,
+		{ headers: { Authorization: `Bearer ${config.keys.supercell.bs}` } }
+	);
 	const stats = await res.json();
-	let embed = null
-	if(res.status === 200) {
-		let president = stats.members.find((m: any) => m.role === 'president')
-		let presidents = stats.members.filter((m: any) => m.role === 'president')
-		let vicePresidents = stats.members.filter((m: any) => m.role === 'vicePresident')
+	let embed = null;
+	if (res.status === 200) {
+		let president = stats.members.find((m: any) => m.role === 'president');
+		let presidents = stats.members.filter(
+			(m: any) => m.role === 'president'
+		);
+		let vicePresidents = stats.members.filter(
+			(m: any) => m.role === 'vicePresident'
+		);
 		embed = new MessageEmbed()
-		.setAuthor(`${Utils.getClubBadgeEmoteById(stats.badgeId)}${stats.name} | ${stats.tag}`)//No idea if that will work if it doesn't will use some emote url method
-		.setDescription(`\`\`\`\n${stats.description}\n\`\`\``)
-		.addField(`**Club Stats**`,
-		`**Trophies:** \`${stats.trophies || '-'}\`${emojis.bsem2.Trophy}\n` +
-		`**Type:** \`${stats.type || '-'}\`\n` +
-		`**Required Trophies:** \`${stats.requiredTrophies || '-'}\`\n` +
-		`**Members:** \`${stats.members.length || '-'}/100\``)//I guess that's the limit?
-		.addField(`**Member's Stats**`,
-		`**Presidents**\n` +
-		`${presidents.map((m: any) => `${Utils.getPlayerBadgeEmoteById(m.icon.id)}${m.name} \`${m.trophies || '-'}\`${emojis.bsem2.Trophy}`).join(`\n`)}\n` +
-		`**Vice Presidents**\n` +
-		`${vicePresidents.map((m: any) => `${Utils.getPlayerBadgeEmoteById(m.icon.id)}${m.name} \`${m.trophies || '-'}\`${emojis.bsem2.Trophy}`).join(`\n`)}\n`)//Will add more once i come to know what actually are the roles.
-		.setColor(stats.members.find('#' + president.nameColor.slice(4)))
-		}
+			.setAuthor(
+				`${Utils.getClubBadgeEmoteById(stats.badgeId)}${stats.name} | ${
+					stats.tag
+				}`
+			) //No idea if that will work if it doesn't will use some emote url method
+			.setDescription(`\`\`\`\n${stats.description}\n\`\`\``)
+			.addField(
+				`**Club Stats**`,
+				`**Trophies:** \`${stats.trophies || '-'}\`${
+					emojis.bsem2.Trophy
+				}\n` +
+					`**Type:** \`${stats.type || '-'}\`\n` +
+					`**Required Trophies:** \`${
+						stats.requiredTrophies || '-'
+					}\`\n` +
+					`**Members:** \`${stats.members.length || '-'}/100\``
+			) //I guess that's the limit?
+			.addField(
+				`**Member's Stats**`,
+				`**Presidents**\n` +
+					`${presidents
+						.map(
+							(m: any) =>
+								`${Utils.getPlayerBadgeEmoteById(m.icon.id)}${
+									m.name
+								} \`${m.trophies || '-'}\`${
+									emojis.bsem2.Trophy
+								}`
+						)
+						.join(`\n`)}\n` +
+					`**Vice Presidents**\n` +
+					`${vicePresidents
+						.map(
+							(m: any) =>
+								`${Utils.getPlayerBadgeEmoteById(m.icon.id)}${
+									m.name
+								} \`${m.trophies || '-'}\`${
+									emojis.bsem2.Trophy
+								}`
+						)
+						.join(`\n`)}\n`
+			) //Will add more once i come to know what actually are the roles.
+			.setColor(stats.members.find('#' + president.nameColor.slice(4)));
+	}
 
 	return {
 		status: res.status,
 		result: res,
-		embed: embed
-	}
+		embed: embed,
+	};
 }
 
 export default {
