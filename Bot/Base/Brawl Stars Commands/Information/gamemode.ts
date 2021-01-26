a8d56928876b88bfdb86e3c71711b38ab6bf8a4d
Dhruvin-Purohit <paragdhruvincoc@gmail.com>
2021-01-26 15:20:13 +0530

    Beeeeaaauuutiiiffyyyyyyyyyy

--- Bot/Base/Brawl Stars Commands/Information/gamemode.ts
+++ Bot/Base/Brawl Stars Commands/Information/gamemode.ts
@@ -2,21 +2,20 @@ import { Command } from 'discord-akairo';
 import { Message } from 'discord.js';
 import index from '../../../index';
 
-
 export default class extends Command {
 	public constructor() {
 		super('brawl_command_information_events', {
-            aliases: ['gamemode', 'gamemodes'],
-            args: [
-                {
-                    id: 'gm',
-                    type: 'string',
-                    match: 'rest',
-                    prompt: {
-                        start: 'What gamemode?'
-                    }
-                }
-            ]
+			aliases: ['gamemode', 'gamemodes'],
+			args: [
+				{
+					id: 'gm',
+					type: 'string',
+					match: 'rest',
+					prompt: {
+						start: 'What gamemode?',
+					},
+				},
+			],
 		});
 	}
 
@@ -30,18 +29,26 @@ export default class extends Command {
 				`Hey, The api utility for this command is not ready yet.\nTry again in a few minutes.`
 			);
 		//The actual needed data
-        let gamemodes = index.starlistapicache?.data.get('gamemodes');
-        let whichOne = gamemodes.find((el: any) => el.name === gm)
+		let gamemodes = index.starlistapicache?.data.get('gamemodes');
+		let whichOne = gamemodes.find((el: any) => el.name === gm);
 
-        if(!whichOne) return message.channel.send(`${message.author}, game mode \`${gm}\` could not be found.\nCheck the name and try again later.`)
-        let embed = this.client.util.embed()
-        .setAuthor(`${whichOne.name}`, whichOne.imageUrl,`${whichOne.link}/?utm_source=discord&utm_campaign=Dhruvin%27s%20Bot`)
-        .setImage(whichOne['imageUrl2'])
-        .setColor(whichOne.color)
-        .setTitle(whichOne.title)
-        .setDescription(`\`\`\`\n${whichOne.tutorial}\n\`\`\``)
-        .addField('\u200b', `\`\`\`\n${whichOne.description}\n\`\`\``)
-        .setFooter(`Information provided by StarList ❤️`)
-        return message.util?.send(embed)
+		if (!whichOne)
+			return message.channel.send(
+				`${message.author}, game mode \`${gm}\` could not be found.\nCheck the name and try again later.`
+			);
+		let embed = this.client.util
+			.embed()
+			.setAuthor(
+				`${whichOne.name}`,
+				whichOne.imageUrl,
+				`${whichOne.link}/?utm_source=discord&utm_campaign=Dhruvin%27s%20Bot`
+			)
+			.setImage(whichOne['imageUrl2'])
+			.setColor(whichOne.color)
+			.setTitle(whichOne.title)
+			.setDescription(`\`\`\`\n${whichOne.tutorial}\n\`\`\``)
+			.addField('\u200b', `\`\`\`\n${whichOne.description}\n\`\`\``)
+			.setFooter(`Information provided by StarList ❤️`);
+		return message.util?.send(embed);
 	}
 }
