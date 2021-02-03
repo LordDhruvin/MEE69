6b17c85d372388039b8510611a6bfff982d3f6ca
Dhruvin-Purohit <paragdhruvincoc@gmail.com>
2021-02-03 15:14:50 +0530

    (Added) A small warning

--- Bot/Data/config.ts
+++ Bot/Data/config.ts
@@ -1,6 +1,5 @@
-/*
- * I know this will error because typescript will try to find the devconfig file when we want to keep it optional.
- * I am looking for a workaround this. If you have any ideas, kindly open an issue or pull request.* * * * * * * *
+/**
+ * This is necessary.
  */
 let devconfig
 try {
