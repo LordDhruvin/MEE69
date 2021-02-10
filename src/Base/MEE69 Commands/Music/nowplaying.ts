05c995666c1c70f9f3beb1d42029b4fb46610e96
Dhruvin-Purohit <paragdhruvincoc@gmail.com>
2021-02-10 11:47:45 +0530

    Added a nowplaying command.

---
+++ src/Base/MEE69 Commands/Music/nowplaying.ts
@@ -0,0 +1,31 @@
+import { Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+
+export default class extends Command {
+	public constructor() {
+		super('command_mee69_nowplaying', {
+			aliases: ['nowplaying', 'np'],
+			channel: 'guild',
+		});
+	}
+	public async exec(message: Message) {
+		if (!message.guild) return 'How the FUCK??';
+		let player = await message.guild?.player();
+		if (!player || !player.queue.current)
+			return message.channel.send(
+				'No song(s) are being played at the moment.',
+			);
+		const { title, author, identifier } = player.queue.current;
+
+		let emb = this.client.util
+			.embed()
+			.setTitle(
+				'[] Currently Playing: {} by ()'
+					.replace('[]', player.playing ? '▶️' : '⏸️')
+					.replace('{}', title)
+					.replace('()', author || 'Unknown'),
+			)
+			.setImage(`http://i3.ytimg.com/vi/${identifier}/maxresdefault.jpg`);
+		return message.util?.send(emb);
+	}
+}
