fe17d7237a2773b6b8f87cdafbe8bfcd977c7ab1
Dhruvin-Purohit <paragdhruvincoc@gmail.com>
2021-02-11 12:52:50 +0530

    (Fail) Tried to change the type of Akairo#Command#description

---
+++ src/Structures/Discord Akairo/Command.ts
@@ -0,0 +1,11 @@
+//Someone help! how to force change types?
+
+/*declare module 'discord-akairo' {
+    interface Command {
+        description?: {
+            short?: string,
+            usage?: string,
+            hidden?: boolean
+        }
+    }
+}*/
