1005dc52ddea89fbed77c4f92733178f413c30c3
dhruvin-purohit <TheLordDhruvin@gmail.com>
2021-12-28 15:12:59 +0000

    Added missing dependencies Removed misleading comment Made shutdown and init methods on Plugin.ts to be optional

--- src/core/plugins/Plugin.ts
+++ src/core/plugins/Plugin.ts
@@ -3,11 +3,9 @@ import type { Promisable } from "../util";
 
 export interface Plugin {
     id: string;
-    init: (bot: Bot) => Promisable<unknown>;
+    init?: (bot: Bot) => Promisable<unknown>;
     load: () => Promisable<unknown>;
     reload: () => Promisable<unknown>;
     unload: () => Promisable<unknown>;
-    destroy: () => Promisable<unknown>;
+    destroy?: () => Promisable<unknown>;
 }
-
-// To use plugins, you construct them first, then pass it to the bot to use it.
